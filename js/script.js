50155fa89db63354ff6c001b025bd8ea1db1ad59
Yo <yohanndore48@gmail.com>
2017-02-16 11:20:14 +0100

    Premier essai de position aleatiore du pigeon

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 var board = document.getElementById("board");
 var pigeon = document.createElement("div");
 pigeon.className = "pigeon";
-div.document.appendChild("pigeon");
+board.appendChild(pigeon);
+
+var pigeonRandom = function(){
+    var position = Math.floor(Math.random() * board.length );
+    return board[position];
+};
+pigeonRandom(pigeon);
