9960b37beecd2830e60fc5a68bb7ba81fd5cfb72
Yo <yohanndore48@gmail.com>
2017-02-16 12:03:11 +0100

    avancé pas top du js

--- js/script.js
+++ js/script.js
@@ -3,8 +3,14 @@ var pigeon = document.createElement("div");
 pigeon.className = "pigeon";
 board.appendChild(pigeon);
 
+// var pigeonRandom = function(){
+//     var position = Math.floor(Math.random() * board.length );
+//     return board[position];
+// };
+// pigeonRandom(pigeon);
+
 var pigeonRandom = function(){
-    var position = Math.floor(Math.random() * board.length );
-    return board[position];
-};
-pigeonRandom(pigeon);
+    positionD = pigeon.style.right + Math.random()* board.length;
+    positionB = pigeon.style.bottom + Math.random()* board.length;
+
+    pigeon =
