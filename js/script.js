bb68647785d17bafe1a40d45def0d3a63bb35ae5
Tibau <xiliar1888@hotmail.fr>
2017-02-16 14:52:54 +0100

    Générer plusieurs pigeons au bout d'un laps de temps (V0.0001)

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
 var board = document.getElementById("board");
 var pigeon = document.createElement("div");
-pigeon.id = "pigeon";
+pigeon.className = "pigeon";
 board.appendChild(pigeon);
-
+pigeon.style.left = Math.floor(Math.random() * board.clientWidth) + "px";
+pigeon.style.top = Math.floor(Math.random() * board.clientHeight) + "px";
 
 setInterval (function () {
     pigeon.style.left = Math.floor(Math.random() * board.clientWidth) + "px";
     pigeon.style.top = Math.floor(Math.random() * board.clientHeight) + "px";
-    },1000);
-    
+},1000);
+
 pigeon.addEventListener("click", function(event){
-    board.removeChild(pigeon);
+    board.removeChild(this);
 });
+
+setInterval (function() {
+    pigeon = document.createElement("div");
+    board.appendChild(pigeon);
+    pigeon.className = "pigeon";
+    pigeon.style.left = Math.floor(Math.random() * board.clientWidth) + "px";
+    pigeon.style.top = Math.floor(Math.random() * board.clientHeight) + "px";
+}, 5000)
