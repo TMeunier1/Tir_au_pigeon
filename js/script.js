313b52d7b948d0542d881be7ef3b2d0bc092484a
Tibau <xiliar1888@hotmail.fr>
2017-02-16 13:28:13 +0100

    Essai d'ajout d'apparition aléatoire

--- js/script.js
+++ js/script.js
@@ -3,14 +3,6 @@ var pigeon = document.createElement("div");
 pigeon.className = "pigeon";
 board.appendChild(pigeon);
 
-// var pigeonRandom = function(){
-//     var position = Math.floor(Math.random() * board.length );
-//     return board[position];
-// };
-// pigeonRandom(pigeon);
 
-var pigeonRandom = function(){
-    positionD = pigeon.style.right + Math.random()* board.length;
-    positionB = pigeon.style.bottom + Math.random()* board.length;
-
-    pigeon =
+pigeon.style.left = Math.floor(Math.random() * board.style.width) +"%";
+pigeon.style.top = Math.floor(Math.random() * board.style.height) +"%";
