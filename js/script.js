f0c442317b0aa5f838f6f853f8462ff1057ac0dd
Tibau <xiliar1888@hotmail.fr>
2017-02-17 15:40:05 +0100

    Ajout du bazooka

--- js/script.js
+++ js/script.js
@@ -6,13 +6,18 @@ setInterval (function() {
     board.appendChild(pigeon);
     pigeon.style.left = Math.floor(Math.random() * board.clientWidth) + "px";
     pigeon.style.top = Math.floor(Math.random() * board.clientHeight) + "px";
-
     setInterval (function () {
         pigeon.style.left = Math.floor(Math.random() * board.clientWidth) + "px";
         pigeon.style.top = Math.floor(Math.random() * board.clientHeight) + "px";
     },1000);
 
-    pigeon.addEventListener("click", function(event){
-        board.removeChild(pigeon);
-    });
+    board.addEventListener("click", function(event){
+        var coordonneesH = event.clientX;
+        var coordonneesV = event.clientY;
+        var pigeonRect = pigeon.getBoundingClientRect();
+        console.log(coordonneesH, coordonneesV, pigeonRect);
+        if (coordonneesH <= pigeonRect.left + 200 && coordonneesH >= pigeonRect.left - 200 && coordonneesV <= pigeonRect.top + 200 && coordonneesV >= pigeonRect.top - 200) {
+            board.removeChild(pigeon);
+        }
+        });
 }, 3000);
