73b0ea6a9f58c4e6cdbe09813703cedf0ea83a21
Tibau <xiliar1888@hotmail.fr>
2017-02-17 14:40:33 +0100

    indentation code

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
 var board = document.getElementById("board");
-setInterval (function() {
-var pigeon = document.createElement("div");
-pigeon.className = "pigeon";
-board.appendChild(pigeon);
-pigeon.style.left = Math.floor(Math.random() * board.clientWidth) + "px";
-pigeon.style.top = Math.floor(Math.random() * board.clientHeight) + "px";
 
-setInterval (function () {
+setInterval (function() {
+    var pigeon = document.createElement("div");
+    pigeon.className = "pigeon";
+    board.appendChild(pigeon);
     pigeon.style.left = Math.floor(Math.random() * board.clientWidth) + "px";
     pigeon.style.top = Math.floor(Math.random() * board.clientHeight) + "px";
-},1000);
-pigeon.addEventListener("click", function(event){
-         board.removeChild(pigeon);
-});
 
+    setInterval (function () {
+        pigeon.style.left = Math.floor(Math.random() * board.clientWidth) + "px";
+        pigeon.style.top = Math.floor(Math.random() * board.clientHeight) + "px";
+    },1000);
+
+    pigeon.addEventListener("click", function(event){
+        board.removeChild(pigeon);
+    });
 }, 3000);
